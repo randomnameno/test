8f46870d81c10b2d9d3c2f7125126ca499ca2b8c
randomnameno <52492367+randomnameno@users.noreply.github.com>
2020-09-09 12:07:30 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import CollegeSite from './Containers/CollegeSite'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component {
+  render()
+  {
+    return(
+      <div>
+          <CollegeSite/>
+      </div>
+    )
+  }
+}
+
+export default App;
